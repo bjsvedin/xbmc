15703b4add099d59d31687b7d10d23071bf003a8
Brady Svedin <brady.svedin@gmail.com>
2026-07-15 22:06:03 -0600

    Add setting to keep all demuxed subtitles in memory for instant switching.

--- xbmc/settings/Settings.h
+++ xbmc/settings/Settings.h
@@ -158,6 +158,7 @@ public:
   static constexpr auto SETTING_LOCALE_SUBTITLELANGUAGE = "locale.subtitlelanguage";
   static constexpr auto SETTING_SUBTITLES_HIDESAMEAUDIOLANGUAGE = "subtitles.hidesameaudiolanguage";
   static constexpr auto SETTING_SUBTITLES_PARSECAPTIONS = "subtitles.parsecaptions";
+  static constexpr auto SETTING_SUBTITLES_KEEPALLDEMUXED = "subtitles.keepalldemuxed";
   static constexpr auto SETTING_SUBTITLES_CAPTIONSALIGN = "subtitles.captionsalign";
   static constexpr auto SETTING_SUBTITLES_CAPTIONSIMPAIRED = "subtitles.captionsimpaired";
   static constexpr auto SETTING_SUBTITLES_ALIGN = "subtitles.align";
